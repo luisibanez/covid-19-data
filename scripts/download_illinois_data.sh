1966f3759349e7eece6102ee319a5cc342e27513
Luis Ibanez <luis.ibanez@gmail.com>
2020-04-09 19:16:54 -0500

    Add download of historical results data.

--- scripts/download_illinois_data.sh
+++ scripts/download_illinois_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl -X GET http://www.dph.illinois.gov/sitefiles/COVIDZip.json?nocache=1 --output ../data/United_States/Illinois/COVIDZip.json
+
+curl -X GET http://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1 --output ../data/United_States/Illinois/COVIDHistoricalTestResults.json
